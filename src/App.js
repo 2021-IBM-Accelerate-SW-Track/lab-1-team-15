ff90a8a87d9b2a8d5a12a3cccc70cd6fe5ae193f
samakinlolu <akinlos1@tcnj.edu>
2021-06-15 18:47:10 -0400

    first commit for group 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
-import Header from "./component/header"
+import Header from "./component/header";
+import React from "react";
 import './App.css';
+import listItems from "./listItems";
 
-function App() {
-  return (
+class App extends React.Component{
+
+ constructor(props){
+  super(props);
+  this.state={
+    items:[],
+    currentItem:{
+      text:'',
+      key:''
+    }
+  }
+  this.handleInput = this.handleInput.bind(this);
+  this.addItem = this.addItem.bind(this);
+ }
+handleInput(e){
+  this.setState({
+    currentItem:{
+      text: e.target.value,
+      key: Date.now()
+    }
+  })
+}
+addItem(e){
+  e.preventDefault(); 
+  const newItem = this.state.currentItem;
+  console.log(newItem);
+    if(newItem.text!==""){
+    const newItems = [...this.state.items, newItem];
+    this.setState({
+      items:newItems,
+      currentItem:{
+        text:'',
+        key:''
+      },
+    })
+  }
+}
+render(){
+ return (
     <div className="App">
       <Header/>
-        <form id = "button">
-          <input type = "text" placeholder="Enter To-Do"/>
-          <button type = "submit">Add To-Do</button>
+        <form id = "to-do-form" onSubmit={this.addItem}>
+          <input type = "text" placeholder="Enter To-Do"
+          value = {this.state.currentItem.text}
+          onChange = {this.handleInput}/>
+          <button type = "submit">Add Item
+          </button>
         </form>
+      <listItems></listItems>  
     </div>
-
   );
+}
 }
 
 export default App;
---
+++ src/listItems.js
@@ -0,0 +1,9 @@
+import react from 'react';
+import './listItems.css'
+
+function listItems(props){
+    return(
+        <h1>From listItems.js</h1>
+    )
+}
+export default listItems; 
