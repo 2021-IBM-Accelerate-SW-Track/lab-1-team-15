7131e61ecee772d296bc982c0d0000db79753561
Dacoyal <dacoyal@gmail.com>
2021-06-18 15:28:05 -0400

    Changed listItemt to ListItems so now we can see the todos in the website

--- src/App.js
+++ src/App.js
@@ -1,46 +1,48 @@
-import Header from "./component/header"
-import './App.css';
+import Header from "./component/header";
 import React from "react";
-import { render } from "react-dom/cjs/react-dom.development";
+import './App.css';
+import listItems from "./listItems";
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {items: [], currentItem: {text: '', key:'' }}
-    this.handleInput = this.handleInput.bind(this);
-    this.addItem = this.addItem.bind(this);
+class App extends React.Component{
+
+ constructor(props){
+  super(props);
+  this.state={
+    items:[],
+    currentItem:{ text:'', key:'' }
   }
-  
-  handleInput(e){
-    this.setState({
-      currentItem: {text: e.target.value, key: Date.now()}
-    })
+  this.handleInput = this.handleInput.bind(this);
+  this.addItem = this.addItem.bind(this);
+ }
+handleInput(e){
+  this.setState({
+    currentItem:{text: e.target.value, key: Date.now()}
+  })
+}
+addItem(e){
+  e.preventDefault(); 
+  const newItem = this.state.currentItem;
+  console.log(newItem);
+  if(newItem.text!==""){
+      const newItems = [...this.state.items, newItem];
+      this.setState({items:newItems, currentItem:{text:'', key:''}})
   }
-  //function that adds items
-  addItem(e){
-    e.preventDefault(); //when we click on add our page will not refresh
-    const newItem = this.state.currentItem;
-    const newItems = [...this.state.items, newItem];
-    this.setState({
-      items: newItems,
-      currentItem: {
-        text: '',
-        key: ''
-      }
-    })
-  }
-  
-  render(){
-    return (
-      <div className="App">
-        <Header/>
-          <form id = "button">
-            <input type = "text" placeholder="Enter To-Do"/>
-            <button type = "submit">Add To-Do</button>
-          </form>
-      </div>
-    );
-  }
+}
+render(){
+ return (
+    <div className="App">
+      <Header/>
+        <form id = "to-do-form" onSubmit={this.addItem}>
+          <input type = "text" placeholder="Enter To-Do"
+          value = {this.state.currentItem.text}
+          onChange = {this.handleInput}/>
+          <button type = "submit">Add Item
+          </button>
+        </form>
+      <listItems items = {this.state.items}></listItems>  
+    </div>
+  );
+}
 }
 
 export default App;
